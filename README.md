095d37a6e4d7f64aa007f4cdd249bfe4466a8227
subin <subinshrestha429@gmail.com>
2021-07-20 15:34:33 +0545

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TaskTracker
 
+Simple Task Tracker using Angular
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.2.
 
 ## Development server
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+	selector: 'app-task-item',
+	templateUrl: './task-item.component.html',
+	styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+	@Input() task: Task = {
+		id: 1,
+		text: 'Doctors Appointment',
+		day: 'May 5th at 2:30pm',
+		reminder: true,
+	};
+
+	@Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+	@Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+	faTimes = faTimes;
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	onDelete(task: Task) {
+		this.onDeleteTask.emit(task);
+	}
+
+	onToggle(task: Task) {
+		this.onToggleReminder.emit(task);
+	}
+}
